522692024d32f6fe64e31137d34b95a5c685b3a6
Phlongs <phlongs@gmail.com>
2017-05-05 15:53:19 -0400

    heroku attempt

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,4 @@
-
+INSERT INTO burgers(burger_name, devoured, date_time)
+VALUES ("Quarter Pounder", false, "2016-11-05 16:20:01"),
+("McRoyal", false, "2016-11-05 04:20:20"),
+("Double Double", true, "2016-11-05 16:20:16");
